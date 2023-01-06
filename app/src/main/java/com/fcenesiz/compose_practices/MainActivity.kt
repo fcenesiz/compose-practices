5794b887e6b4f6cf2577fb7049547ff714099881
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2023-01-06 14:58:37 +0300

    rows, columns & basic sizing

--- app/src/main/java/com/fcenesiz/compose_practices/MainActivity.kt
+++ app/src/main/java/com/fcenesiz/compose_practices/MainActivity.kt
@@ -3,41 +3,50 @@ package com.fcenesiz.compose_practices
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fcenesiz.compose_practices.ui.theme.ComposePracticesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePracticesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(1f)
+                    .background(Color.DarkGray),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Title()
+                Text(
+                    color = Color.White,
+                    text = "This is test 1"
+                )
+                Text(text = "This is text 2")
+
             }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposePracticesTheme {
-        Greeting("Android")
+fun Title() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Compose is")
+        Text(text = "very Cool!")
     }
 }
